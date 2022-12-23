4913621d9bd4f730ae9299d1490fe2fda75edda9
CarlosLes <CarlosLes@users.noreply.github.com>
2022-12-23 14:00:28 +0100

    update tests for new requirements

--- tests/unit/test_calculator.py
+++ tests/unit/test_calculator.py
@@ -4,9 +4,15 @@ from hypothesis import strategies as st
 from spi.calculator import interpret
 
 
-@given(st.integers(0, 9), st.integers(0, 9), st.sampled_from(['+', '-']))
-def test_interpret_operators(x: int, y: int, op: str) -> None:
-    text = f'{x}{op}{y}'
+@given(st.integers(0))
+def test_interpret_single_integer(x: int) -> None:
+    text = str(x)
+    assert interpret(text) == x
+
+
+@given(st.integers(0), st.integers(0), st.sampled_from(['+', '-']))
+def test_interpret_integer_pair(x: int, y: int, op: str) -> None:
+    text = f'{x} {op} {y}'
     if op == '+':
         assert interpret(text) == x + y
     elif op == '-':
@@ -15,23 +21,7 @@ def test_interpret_operators(x: int, y: int, op: str) -> None:
         raise ValueError(f'unexpected operator "{op}"')
 
 
-@given(st.integers(0, 9), st.integers(0, 9), st.sampled_from(['+', '-']))
-def test_interpret_with_whitespaces(x: int, y: int, op: str) -> None:
-    text = f'{x} {op}  {y}'
-    if op == '+':
-        assert interpret(text) == x + y
-    elif op == '-':
-        assert interpret(text) == x - y
-    else:
-        raise ValueError(f'unexpected operator "{op}"')
-
-
-@given(st.integers(min_value=10), st.integers(min_value=0), st.sampled_from(['+', '-']))
-def test_interpret_with_multidigit_ints(x: int, y: int, op: str) -> None:
-    text = f'{x}{op}{y}'
-    if op == '+':
-        assert interpret(text) == x + y
-    elif op == '-':
-        assert interpret(text) == x - y
-    else:
-        raise ValueError(f'unexpected operator "{op}"')
+@given(st.lists(st.integers(0), min_size=4, max_size=4))
+def test_interpret_four_integers(xs: list[int]) -> None:
+    text = f'{xs[0]} + {xs[1]} - {xs[2]} - {xs[3]}'
+    assert interpret(text) == xs[0] + xs[1] - xs[2] - xs[3]
