7591523dc98c4513ec0dcb2f5554884f3debae51
CarlosLes <CarlosLes@users.noreply.github.com>
2022-12-31 20:32:00 +0100

    refactor lexer to be more purely functional

--- src/spi/lexer.py
+++ src/spi/lexer.py
@@ -1,33 +1,40 @@
 from collections.abc import Iterator
-from itertools import pairwise, repeat
+from itertools import chain, repeat
+
+from more_itertools import before_and_after, first
 
 from spi.token import Token, TokenType
 
 
-def tokenize(text: str) -> Iterator[Token]:
+def tokenize(chars: str) -> Iterator[Token]:
     """Lexically analyze (also known as scan or tokenize) input source."""
-    digits: list[str] = []
-    for char, next_char in pairwise(text + ' '):
-        if char.isspace():
-            continue
-        match char:
-            case c if c.isdigit():
-                digits += char
-                if not next_char.isdigit():
-                    yield Token(TokenType.INTEGER, int(''.join(digits)))
-                    digits = []
-            case '+':
-                yield Token(TokenType.PLUS, char)
-            case '-':
-                yield Token(TokenType.MINUS, char)
-            case '*':
-                yield Token(TokenType.MUL, char)
-            case '/':
-                yield Token(TokenType.DIV, char)
-            case '(':
-                yield Token(TokenType.LPAREN, char)
-            case ')':
-                yield Token(TokenType.RPAREN, char)
-            case _:
-                raise ValueError(f'error parsing input "{char}"')
+    yield from itokenize(iter(chars))
     yield from repeat(Token(TokenType.EOF, None))
+
+
+def itokenize(chars: Iterator[str]) -> Iterator[Token]:
+    char = first(chars, default=None)
+    match char:
+        case None:
+            return
+        case ' ':
+            pass  # do nothing
+        case '+':
+            yield Token(TokenType.PLUS, char)
+        case '-':
+            yield Token(TokenType.MINUS, char)
+        case '*':
+            yield Token(TokenType.MUL, char)
+        case '/':
+            yield Token(TokenType.DIV, char)
+        case '(':
+            yield Token(TokenType.LPAREN, char)
+        case ')':
+            yield Token(TokenType.RPAREN, char)
+        case c if c.isdigit():
+            digits, chars = before_and_after(str.isdigit, chars)
+            integer = int(''.join(chain(char, digits)))
+            yield Token(TokenType.INTEGER, integer)
+        case _:
+            raise ValueError(f'error parsing input "{char}"')
+    yield from itokenize(chars)
